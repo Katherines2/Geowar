5abebe2c7a6e946d4fc08dbac31f8afbfb7f0423
Katherines2 <132277953+Katherines2@users.noreply.github.com>
2023-12-15 09:42:38 +0300

    Update Makefile

--- makefile.mk
+++ makefile.mk
@@ -5,6 +5,18 @@ SRC = Geowar.cpp
 
 OBJ = $(SRC:.cpp=.o)
 
+windows:
+	mkdir $(WIN)
+	g++ -o Geowar.exe Geowar.cpp
+
+linux:
+	mkdir $(LIN)
+	g++ -o Geowar Geowar.cpp
+
+web:
+	cmd /c start http://localhost:8000/geowar.html
+	python -m http.server 8000
+
 exe: $(OBJ)
     $(CXX) $(CXXFLAGS) -o exe $(OBJ)
 
